f343c4067b783e96cb8c0934a97064908d923842
solveeaseleads-byte <solveease.leads@gmail.com>
2026-09-25 10:47:56 +0100

    Create App component with Dashboard integration

---
+++ (src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Dashboard from "./components/Dashboard.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
